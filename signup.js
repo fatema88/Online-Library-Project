350f692599dd07b5cddc4e431062bd834df07bd9
SeifAzab <sifhana@gmail.com>
2026-04-12 15:31:55 +0200

    Add files via upload

---
+++ signup.js
@@ -0,0 +1,11 @@
+document.getElementById("signupForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const role = document.querySelector('input[name="role"]:checked').value;
+
+    if (role === "admin") {
+        window.location.href = "add_edit_book.html";
+    } else {
+        window.location.href = "manage_books.html";
+    }
+});
